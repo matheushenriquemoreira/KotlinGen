cf6bd967921191d7238def09bf61542bebab580c
matheus henrique <matheus.hen857@hotmail.com>
2022-08-26 19:59:20 -0300

    projeto

---
+++ trabalho1/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+   println("Digite sua data de nascimento")
+  val dia = readln().toInt()
+    val mes = readln().toInt()
+    val ano = readln().toInt()
+
+   if(ano<2004){
+       val dia = true
+       val mes = true
+       val ano = true
+
+   }else if(ano==2004)
+
+}
